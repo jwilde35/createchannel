91e91afaa15d7e1d0458ac4c307528138d91d897
jwilde35 <jwilde35@gmail.com>
2019-10-23 17:34:26 -0400

    Update and rename rndstatus.py to createchannel.py

---
+++ createchannel.py
@@ -0,0 +1,21 @@
+from redbot.core import commands
+from redbot.core import checks
+from redbot.core import Config
+import os
+import time
+
+BaseCog = getattr(commands, "Cog", object)
+
+class createchannel(BaseCog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.group(pass_context=True)
+    @checks.is_owner()
+    async def create(self, ctx):
+        pass
+
+    @createchannel.command(name="channel", pass_context=True, no_pm=True)
+    async def _channel(self, ctx, name : str):
+        guild = ctx.message.guild
+        await guild.create_text_channel(name)
+        await ctx.send("done")
